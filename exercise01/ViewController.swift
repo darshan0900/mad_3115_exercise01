04d859e5b45d0184f0f632aa724f84cf982e13fb
Darshan Jain <c0872398@mylambton.ca>
2022-10-13 19:25:10 -0400

    added ui and functionality;

--- exercise01/ViewController.swift
+++ exercise01/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var heading: UILabel!{
+		didSet{
+			heading.sizeToFit()
+		}
+	}
+	
+	@IBOutlet weak var changeBgColor: UIButton!
+	
+	@IBOutlet weak var hintLabel: UILabel!{
+		didSet{
+			hintLabel.sizeToFit()
+		}
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
 
-
+	
+	@IBAction func onChangeBgColorPress(_ sender: Any) {
+		let redColor: CGFloat = CGFloat.random(in: 0...1)
+		let greenColor: CGFloat = CGFloat.random(in: 0...1)
+		let blueColor: CGFloat = CGFloat.random(in: 0...1)
+		
+		let randomBgColor = UIColor( red: redColor, green: greenColor, blue: blueColor, alpha: 1 )
+		view.backgroundColor = randomBgColor
+		
+		let randomTextColor = UIColor( red: 1.0 - redColor, green: 1.0 - greenColor, blue: 1.0 - blueColor, alpha: 1 )
+		
+		heading.textColor = randomTextColor
+		hintLabel.textColor = randomTextColor
+	}
 }
 
